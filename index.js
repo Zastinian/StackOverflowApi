72198db892f9bd970fd965ae70d9bf0c20931e82
Zastinian <zastinian@gmail.com>
2023-06-29 20:33:12 -0600

    feat: Code

--- index.js
+++ index.js
@@ -1,53 +1,42 @@
-import puppeteer from "puppeteer-core";
-import edgeChromium from "chrome-aws-lambda";
+import cheerio from "cheerio";
 import express from "express";
 
 const app = express();
 
-const executablePath = await edgeChromium.executablePath;
-
-const browser = await puppeteer.launch({
-  executablePath,
-  args: edgeChromium.args,
-  headless: false,
-});
-const page = await browser.newPage();
-
 async function getQuestions(tag, page_number) {
+  let url = "";
   if (tag == null) {
-    await page.goto(`https://stackoverflow.com/questions?tab=active&page=${page_number}`);
+    url = `https://stackoverflow.com/questions?tab=active&page=${page_number}`;
   } else {
-    await page.goto(`https://stackoverflow.com/questions/tagged/${tag}?tab=active&page=${page_number}`);
+    url = `https://stackoverflow.com/questions/tagged/${tag}?tab=active&page=${page_number}`;
   }
 
-  const divs = await page.evaluate(() => {
-    const divElements = Array.from(document.querySelectorAll('div[id^="question-summary-"]'));
+  const response = await fetch(url);
+  const html = await response.text();
 
-    const divs = divElements.map((divElement) => {
-      return {id: divElement.id};
-    });
-
-    return divs;
-  });
+  const $ = cheerio.load(html);
+  const divs = $('div[id^="question-summary-"]')
+    .map((index, element) => {
+      return {id: $(element).attr("id")};
+    })
+    .get();
 
   return divs;
 }
 
 async function getQuestionAndAnswerById(questionId) {
-  await page.goto(`https://stackoverflow.com/questions/${questionId}`);
+  const url = `https://stackoverflow.com/questions/${questionId}`;
+  const response = await fetch(url);
+  const html = await response.text();
 
-  const question = await page.evaluate(() => {
-    const questionTitle = document.querySelector(".question-hyperlink").innerText;
-    const questionBody = document.querySelector(".js-post-body").innerText;
+  const $ = cheerio.load(html);
+  const questionTitle = $(".question-hyperlink").text();
+  const questionBody = $(".js-post-body").text();
+  const answerBody = $(".js-post-body").first().text();
 
-    return {title: questionTitle, body: questionBody};
-  });
+  const question = {title: questionTitle, body: questionBody};
+  const answer = {body: answerBody};
 
-  const answer = await page.evaluate(() => {
-    const answerBody = document.querySelector(".js-post-body").innerText;
-
-    return {body: answerBody};
-  });
   return {question, answer};
 }
 
@@ -59,7 +48,7 @@ app.get("/", async (req, res) => {
     for (let i = 0; i < questions.length; i++) {
       const questionId = questions[i].id.replace("question-summary-", "");
       const {question, answer} = await getQuestionAndAnswerById(questionId);
-
+      if (!answer) return;
       responseData.push({
         id: questionId,
         title: question.title,
