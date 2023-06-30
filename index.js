c7cb8369c76aab3241af5448a7567f1ceb550343
Zastinian <zastinian@gmail.com>
2023-06-29 20:12:08 -0600

    feat: Args

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ const app = express();
 
 const browser = await launch({
   headless: "true",
+  args: [
+    "--disable-voice-input",
+    "--no-default-browser-check",
+    "--disable-translate",
+    "--disable-sync",
+    "--disable-site-isolation-trials",
+    "--disable-renderer-backgrounding",
+    "--disable-infobars",
+    "--disable-remote-fonts",
+    "--disable-logging",
+    "--disable-hang-monitor",
+    "--disable-default-apps",
+    "--disable-breakpad",
+    "--headless=new",
+    "--no-sandbox",
+  ],
 });
 const page = await browser.newPage();
 
